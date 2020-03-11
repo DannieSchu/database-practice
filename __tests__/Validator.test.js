5bd84d81d7b9169647971fcd25a87922d8de66cf
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-10 17:34:55 -0700

    validate proper type

--- __tests__/Validator.test.js
+++ __tests__/Validator.test.js
@@ -6,11 +6,24 @@ describe('Validator', () => {
       type: String,
       required: true
     });
-  
+    
     expect(nameValidator.field).toEqual('name');
     expect(nameValidator.configuration).toEqual({
       type: String,
       required: true
     });
   });
+
+  it('can validate an object of the proper type', () => {
+    const nameValidator = new Validator('name', {
+      type: String,
+      required: true
+    });
+    const dog = {
+      name: 'Hobbes',
+      age: 23,
+      weight: '15 lbs'
+    };
+    expect(nameValidator.validate(dog)).toEqual('Hobbes');
+  })
 });
