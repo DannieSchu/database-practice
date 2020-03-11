9bd1bf8b1e5bb95db6c072ee332b2218efba3021
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-10 22:03:10 -0700

    schema returns object if weight is missing

--- __tests__/Schema.test.js
+++ __tests__/Schema.test.js
@@ -7,12 +7,13 @@ describe('Schema', () => {
       age: Number,
       weight: String
     });
+
     const spot = {
       name: 'spot',
       age: 5,
       weight: '20 lbs'
     };
-    
+
     expect(dogSchema.validate(spot)).toEqual({
       name: 'spot',
       age: 5,
@@ -20,11 +21,36 @@ describe('Schema', () => {
     });
   });
 
-  // it('throws an error if object does not follow schema', () => {
+  it('returns object if optional values are missing', () => {
+    const dogSchema = new Schema('dog', {
+      name: String,
+      age: Number,
+      weight: String
+    });
 
-  // });
+    const rover = {
+      name: 'rover',
+      age: 5,
+    };
+
+    expect(dogSchema.validate(rover)).toEqual({
+      name: 'rover',
+      age: 5
+    });
+  });
+
+
+  it('throws an error if object does not follow schema', () => {
+    const dogSchema = new Schema('dog', {
+      name: String,
+      age: Number,
+      weight: String
+    });
+
+    const who = {
+      age: 'hi'
+    };
+
+    expect(() => dogSchema.validate(who).toThrowError('Cannot cast >>[object Object]<< to String'));
+  });
 });
-
-// use the object to create an array of Validators for each key/value in schemaDefinition and store the array in this.validators 
-
-//Schema has a validate method that takes an object
