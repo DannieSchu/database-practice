833ce0322ec8e396cfb74d45d869944dc0b4d6b7
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-14 14:38:28 -0700

    write and test function to find and update dog

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -114,27 +114,63 @@ describe('Model class', () => {
     // Dog class is a new model with name 'Dog' in shape of schema
     const Dog = new Model('Dog', schema);
 
+    
     return Dog
+      // find all dogs
       .find()
+      // then expect all dogs to equal an array of dogs that each follow schema
       .then(allDogs => {
-        expect(allDogs).toEqual([{
+        const mappedDogs = allDogs.map(dog => {
+          return {
+            _id: expect.any(String),
+            name: dog.name,
+            age: dog.age,
+            weight: dog.weight
+          };
+        }
+        );
+        expect(allDogs).toEqual(mappedDogs);
+      });  
+  });
+
+  it('updates a dog', () => {
+    const schema = new Schema({
+      name: {
+        type: String,
+        required: true
+      },
+      age: {
+        type: Number,
+        required: true
+      },
+      weight: {
+        type: String
+      }
+    });  
+  
+    // Dog class is a new model with name 'Dog' in shape of schema
+    const Dog = new Model('Dog', schema);
+      
+    return Dog
+      .create({
+        name: 'spot',
+        age: 5,
+        weight: '20 lbs'
+      })
+      
+    // find all dogs      
+      .then(dog => {
+        return Dog
+          .findByIdAndUpdate(dog._id, { name: 'rover' });
+      })
+      
+      .then(updatedDog => {
+        expect(updatedDog).toEqual({
           _id: expect.any(String),
-          name: expect.any(String),
-          age: expect.any(Number),
-          weight: expect.any(String)
-        }]);
+          name: 'rover',
+          age: 5,
+          weight: '20 lbs'
+        });
       });
   });
 });
-// .then(dog => {
-//   return Dog
-//     .findByIdAndUpdate(dog._id, { name: 'rover' });
-// })
-
-// .then(updatedDog => {
-//   expect(updatedDog).toEqual({
-//     _id: expect.any(String),
-//     name: 'rover',
-//     age: 5,
-//     weight: '20 lbs'
-//   });
