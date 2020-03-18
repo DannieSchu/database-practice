bfb8a240d612a2e3ff206fceede8f321db38f5bf
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-17 21:24:56 -0700

    delete clearDir for Travis

--- __tests__/model.test.js
+++ __tests__/model.test.js
@@ -1,15 +1,11 @@
 // get model
 const Model = require('../lib/Model');
 const Schema = require('../lib/Schema');
-const { clearDir } = require('../lib/file-system');
-
 
 describe('Model class', () => {
   let schema;
 
-  describe('Model methods', () => {
-    beforeEach(() => clearDir('Dog'));
-  
+  describe('Model methods', () => {  
     beforeAll(() => {
       schema = new Schema({
         name: {
